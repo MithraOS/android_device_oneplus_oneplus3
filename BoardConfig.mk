14c92341f8e4c013a08a43705af0c9f595463e25
joshuous <joshuous@gmail.com>
2017-02-11 18:33:50 +0000

    op3: BoardConfig: Build kernel with Linaro 6.x toolchains
    Change-Id: Ibb97a10c14e087efd40d370566d563709eeb46de

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -42,8 +42,8 @@ TARGET_USE_SDCLANG := true
 #TARGET_ROM_CUSTOM_TOOLCHAIN_AARCH64 := aarch64-linux-android-uber-5.x
 
 # Custom Kernel Toolchains
-#TARGET_KERNEL_CUSTOM_TOOLCHAIN := aarch64-linux-android-linaro-4.9
-#KERNEL_TOOLCHAIN := $(ANDROID_BUILD_TOP)/prebuilts/gcc/$(HOST_OS)-x86/aarch64/$(TARGET_KERNEL_CUSTOM_TOOLCHAIN)/bin
+TARGET_KERNEL_CUSTOM_TOOLCHAIN := aarch64-linux-android-linaro-6.x-kernel
+KERNEL_TOOLCHAIN := $(ANDROID_BUILD_TOP)/prebuilts/gcc/$(HOST_OS)-x86/aarch64/$(TARGET_KERNEL_CUSTOM_TOOLCHAIN)/bin
 
 # Bootloader
 TARGET_BOOTLOADER_BOARD_NAME := msm8996
